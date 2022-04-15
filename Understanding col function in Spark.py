86e572b2bc72b60be701b64bd0228db32dacc609
Dileep <35377950+Dileep90@users.noreply.github.com>
2022-04-15 10:39:27 +0800

    Update Understanding col function in Spark.py

--- Understanding col function in Spark.py
+++ Understanding col function in Spark.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 spark=SparkSession.builder.appName('Order').getOrCreate()
 #orders = spark.read.schema(schema).csv(f'/user/{username}/retail_db_pipe/orders', sep='|')
-df_date=spark.read.format('csv').options(inferSchema='True').load('/FileStore/tables/orders')
+df_date=spark.read.format('csv').options(inferSchema='True').load('orders')
 #schama=('id' int,'date' date,'balance' int, 'status' string)
 #df_date.show()
 df_modi= df_date.select(col('_c0'),date_format('_c1','DDMONYYYY'))
